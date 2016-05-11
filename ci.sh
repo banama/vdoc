a8727fd50c7c3f9a74bf8f476215d9ea2ea94972
banama <306672962@qq.com>
2016-05-11 16:17:04 +0800

    add ci

---
+++ ci.sh
@@ -0,0 +1,11 @@
+set -e
+if [ -z "$CI_PULL_REQUEST" ]
+then
+  npm run lint
+  npm run build
+  npm run cover
+  cat ./coverage/lcov.info | ./node_modules/.bin/codecov
+  npm run sauce-all
+else
+  npm test
+fi
